c8ada5468d6869c87c2924d2b29f3c87852a7008
Andrews <andrewsviana28@gmail.com>
2025-02-19 23:22:52 -0300

    USANDO API

--- app/src/main/java/com/example/meutea/datasource/DataSource.kt
+++ app/src/main/java/com/example/meutea/datasource/DataSource.kt
@@ -1,3 +1,5 @@
+package com.example.meutea.datasource
+
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import kotlinx.coroutines.tasks.await
@@ -7,7 +9,10 @@ class DataSource {
     private val auth: FirebaseAuth = FirebaseAuth.getInstance()
 
     suspend fun signUp(nome: String, email: String, senha: String): FirebaseUser? {
+        // Usando a instância já criada
         val result = auth.createUserWithEmailAndPassword(email, senha).await()
+
+        // Atualizando o nome de exibição do usuário
         result.user?.let { user ->
             user.updateProfile(
                 com.google.firebase.auth.UserProfileChangeRequest.Builder()
@@ -15,6 +20,7 @@ class DataSource {
                     .build()
             ).await()
         }
+
         return result.user
     }
 
@@ -23,3 +29,4 @@ class DataSource {
         return result.user
     }
 }
+
